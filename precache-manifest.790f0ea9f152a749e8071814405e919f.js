fa490e02ab0e3a8cfca499a0ec60cb3602133d25
Naor Fahima <naorfahima96@gmail.com>
2022-03-19 14:58:39 +0200

    Updates

---
+++ precache-manifest.790f0ea9f152a749e8071814405e919f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed79e7950785d7ec83e89d60eef31a7",
+    "url": "/Cyber-System-Detection-Client/index.html"
+  },
+  {
+    "revision": "24eac9d706fd4936898b",
+    "url": "/Cyber-System-Detection-Client/static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "24eac9d706fd4936898b",
+    "url": "/Cyber-System-Detection-Client/static/js/2.ab7e4eba.chunk.js"
+  },
+  {
+    "revision": "ff0a94dfe80ec99bfeab0a4326a8fd50",
+    "url": "/Cyber-System-Detection-Client/static/js/2.ab7e4eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c6ef752ffb22673fa3",
+    "url": "/Cyber-System-Detection-Client/static/js/main.9e31fa6f.chunk.js"
+  },
+  {
+    "revision": "6466a13fbe30b63003ac",
+    "url": "/Cyber-System-Detection-Client/static/js/runtime-main.64f3de4f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Cyber-System-Detection-Client/precache-manifest.790f0ea9f152a749e8071814405e919f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cyber-System-Detection-Client/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
