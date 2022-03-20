b182ccb5f10cd833dd9589f37691c2cb09dbbb46
Gal <birkagal@gmail.com>
2022-03-20 19:36:01 +0200

    Updates

---
+++ precache-manifest.e068c4c5122992873b30742ecff4a6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6f85eb06d472de1c75a8b95da877d7",
+    "url": "/Cyber-System-Detection-Client/index.html"
+  },
+  {
+    "revision": "2f7bd8d3a581e73bbbb0",
+    "url": "/Cyber-System-Detection-Client/static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "2f7bd8d3a581e73bbbb0",
+    "url": "/Cyber-System-Detection-Client/static/js/2.0cc48d39.chunk.js"
+  },
+  {
+    "revision": "ff0a94dfe80ec99bfeab0a4326a8fd50",
+    "url": "/Cyber-System-Detection-Client/static/js/2.0cc48d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7528f9cdd7bb4d98a6",
+    "url": "/Cyber-System-Detection-Client/static/js/main.1f744997.chunk.js"
+  },
+  {
+    "revision": "9f18d91e097c19de3a35",
+    "url": "/Cyber-System-Detection-Client/static/js/runtime-main.68f77ca3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.502befb863009aeb949c287da14c0a94.js"
+  "/Cyber-System-Detection-Client/precache-manifest.e068c4c5122992873b30742ecff4a6db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cyber-System-Detection-Client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
